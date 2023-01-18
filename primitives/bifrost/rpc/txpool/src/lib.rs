8cc2801a8874004032b0112e8ea52fae06e72ddf
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-01-18 13:01:27 +0900

    add bifrost related packages

---
+++ primitives/bifrost/rpc/txpool/src/lib.rs
@@ -0,0 +1,36 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+// These clippy lints are disabled because the macro-generated code triggers them.
+#![allow(clippy::unnecessary_mut_passed)]
+#![allow(clippy::too_many_arguments)]
+
+use codec::{Decode, Encode};
+pub use ethereum::{TransactionV0 as LegacyTransaction, TransactionV2 as Transaction};
+use sp_runtime::traits::Block as BlockT;
+use sp_std::vec::Vec;
+
+#[derive(Eq, PartialEq, Clone, Encode, Decode, sp_runtime::RuntimeDebug)]
+pub struct TxPoolResponseLegacy {
+	pub ready: Vec<LegacyTransaction>,
+	pub future: Vec<LegacyTransaction>,
+}
+
+#[derive(Eq, PartialEq, Clone, Encode, Decode, sp_runtime::RuntimeDebug)]
+pub struct TxPoolResponse {
+	pub ready: Vec<Transaction>,
+	pub future: Vec<Transaction>,
+}
+
+sp_api::decl_runtime_apis! {
+	#[api_version(2)]
+	pub trait TxPoolRuntimeApi {
+		#[changed_in(2)]
+		fn extrinsic_filter(
+			xt_ready: Vec<<Block as BlockT>::Extrinsic>,
+			xt_future: Vec<<Block as BlockT>::Extrinsic>,
+		) -> TxPoolResponseLegacy;
+		fn extrinsic_filter(
+			xt_ready: Vec<<Block as BlockT>::Extrinsic>,
+			xt_future: Vec<<Block as BlockT>::Extrinsic>,
+		) -> TxPoolResponse;
+	}
+}
