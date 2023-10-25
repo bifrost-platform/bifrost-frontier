1bebd4a4ff3751bf9aed9112fae8650f09073322
Alex Won <dnjscksdn98@gmail.com>
2023-10-25 17:42:55 +0900

    chore: remove fungible trait usage (#5)
    * fix .
    
    * fix .
    
    * update deps

--- frame/evm/src/lib.rs
+++ frame/evm/src/lib.rs
@@ -84,7 +84,6 @@ use frame_support::{
 		fungible::{Balanced, Credit, Debt},
 		tokens::{
 			currency::Currency,
-			fungible::Inspect,
 			imbalance::{Imbalance, OnUnbalanced, SignedImbalance},
 			ExistenceRequirement, Fortitude, Precision, Preservation, WithdrawReasons,
 		},
@@ -912,7 +911,7 @@ impl<T: Config> Pallet<T> {
 		let account_id = T::AddressMapping::into_account_id(*address);
 		let nonce = T::AccountProvider::account_nonce(&account_id);
 		let balance =
-			T::Currency::evm_reducible_balance(&account_id, Preservation::Preserve, Fortitude::Polite);
+			T::Currency::reducible_balance(&account_id, Preservation::Preserve, Fortitude::Polite);
 
 		(
 			Account {
