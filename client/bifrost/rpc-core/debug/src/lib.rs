f7668389d933c48faab13b5652fc535ae53b5856
dnjscksdn98 <dnjscksdn98@gmail.com>
2024-07-22 13:51:22 +0900

    feat: update bifrost rpc

--- client/bifrost/rpc-core/debug/src/lib.rs
+++ client/bifrost/rpc-core/debug/src/lib.rs
@@ -1,5 +1,23 @@
-use ethereum_types::H256;
+// Copyright 2019-2022 PureStake Inc.
+// This file is part of Moonbeam.
+
+// Moonbeam is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Moonbeam is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Moonbeam.  If not, see <http://www.gnu.org/licenses/>.
+
+use ethereum::AccessListItem;
+use ethereum_types::{H160, H256, U256};
 use fc_evm_tracing::types::single;
+use fc_rpc_core::types::Bytes;
 use fc_rpc_core_types::RequestBlockId;
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
 use serde::Deserialize;
@@ -15,6 +33,34 @@ pub struct TraceParams {
 	pub timeout: Option<String>,
 }
 
+#[derive(Debug, Clone, Default, Eq, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TraceCallParams {
+	/// Sender
+	pub from: Option<H160>,
+	/// Recipient
+	pub to: H160,
+	/// Gas Price, legacy.
+	pub gas_price: Option<U256>,
+	/// Max BaseFeePerGas the user is willing to pay.
+	pub max_fee_per_gas: Option<U256>,
+	/// The miner's tip.
+	pub max_priority_fee_per_gas: Option<U256>,
+	/// Gas
+	pub gas: Option<U256>,
+	/// Value of transaction in wei
+	pub value: Option<U256>,
+	/// Additional data sent with transaction
+	pub data: Option<Bytes>,
+	/// Nonce
+	pub nonce: Option<U256>,
+	/// EIP-2930 access list
+	pub access_list: Option<Vec<AccessListItem>>,
+	/// EIP-2718 type
+	#[serde(rename = "type")]
+	pub transaction_type: Option<U256>,
+}
+
 #[rpc(server)]
 #[jsonrpsee::core::async_trait]
 pub trait Debug {
@@ -22,6 +68,13 @@ pub trait Debug {
 	async fn trace_transaction(
 		&self,
 		transaction_hash: H256,
+		params: Option<TraceParams>,
+	) -> RpcResult<single::TransactionTrace>;
+	#[method(name = "debug_traceCall")]
+	async fn trace_call(
+		&self,
+		call_params: TraceCallParams,
+		id: RequestBlockId,
 		params: Option<TraceParams>,
 	) -> RpcResult<single::TransactionTrace>;
 	#[method(name = "debug_traceBlockByNumber", aliases = ["debug_traceBlockByHash"])]
